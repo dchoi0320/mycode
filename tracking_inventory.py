623c5cb799eed94637347488a20d4d7f160df9d1
David C <dchoi1022@gmail.com>
2021-12-03 16:46:03 -0500

    42. The Multiprocessing Module

---
+++ tracking_inventory.py
@@ -0,0 +1,124 @@
+# https://github.com/csfeeser/Python/blob/master/pyapi/Tracking_Inventory_challenge_solution.md
+
+```html
+<!doctype html>
+<html>
+   <body>
+      <h2>Welcome to my Flask & database app</h2>
+      <a href = "/">Home Page (you are here)</a>
+      <a href = "/enternew">Make a New Database Entry</a>
+      <a href = "/delete">Delete a Database Entry</a>
+      <a href = "/list">View Records in the Database</a>
+   </body>
+</html>
+```
+
+```html
+**<html>
+   <body>
+      <form action = "/delrec" method = "POST">
+         <h3>Student Information</h3>
+         Name:<br>
+	 <input type = "text" name = "nm" /><br>
+         <input type = "submit" value = "submit" /><br>
+      </form>
+   </body>
+</html>**
+```
+
+```python
+#!/usr/bin/python3
+"""solution provided by DeVon Williams"""
+
+# standard library
+import sqlite3 as sql
+
+# python3 -m pip install flask
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+# return home.html (landing page)
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# return student.html (a way to add a student to our sqliteDB)
+@app.route('/enternew')
+def new_student():
+    return render_template('student.html')
+
+@app.route("/delete")
+def delete_student():
+    return render_template('delete.html')
+
+@app.route('/delrec', methods = ['POST'])
+def delrec():
+    con = sql.connect("database.db")
+    con.row_factory = sql.Row
+    name = request.form['nm']
+    cur = con.cursor()
+    cur.execute(f"DELETE FROM students WHERE name='{name}'")
+    con.commit()
+    con.close()     # successful or not, close the connection to sqliteDB
+    return render_template("result.html",msg = "Entry Deleted!")
+
+# if someone uses student.html it will generate a POST
+# this post will be sent to /addrec
+# where the information will be added to the sqliteDB
+@app.route('/addrec',methods = ['POST'])
+def addrec():
+    try:
+        nm = request.form['nm']         # student name
+        addr = request.form['addr']     # student street address
+        city = request.form['city']     # student city
+        pin = request.form['pin']       # "pin" assigned to student
+                                        # ("pin" is just an example of meta data we want to track)
+
+        # connect to sqliteDB
+        with sql.connect("database.db") as con:
+            cur = con.cursor()
+
+            # place the info from our form into the sqliteDB
+            cur.execute("INSERT INTO students (name,addr,city,pin) VALUES (?,?,?,?)",(nm,addr,city,pin) )
+            # commit the transaction to our sqliteDB
+            con.commit()
+        # if we have made it this far, the record was successfully added to the DB
+        msg = "Record successfully added"
+        
+    except:
+        con.rollback()  # this is the opposite of a commit()
+        msg = "error in insert operation"    # we were NOT successful
+
+    finally:
+        con.close()     # successful or not, close the connection to sqliteDB
+        return render_template("result.html",msg = msg)    #
+
+# return all entries from our sqliteDB as HTML
+@app.route('/list')
+def list_students():
+    con = sql.connect("database.db")
+    con.row_factory = sql.Row
+    
+    cur = con.cursor()
+    cur.execute("SELECT * from students")           # pull all information from the table "students"
+    
+    rows = cur.fetchall()
+    return render_template("list.html",rows = rows) # return all of the sqliteDB info as HTML
+
+if __name__ == '__main__':
+    try:
+        # ensure the sqliteDB is created
+        con = sql.connect('database.db')
+        print("Opened database successfully")
+        # ensure that the table students is ready to be written to
+        con.execute('CREATE TABLE IF NOT EXISTS students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
+        print("Table created successfully")
+        con.close()
+        # begin Flask Application 
+        app.run(host="0.0.0.0", port=2224, debug = True)
+    except:
+        print("App failed on boot")
+```
