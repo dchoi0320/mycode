338c950abec80589974466742b97a3e50aeeecc6
David C <dchoi1022@gmail.com>
2021-12-09 20:48:13 -0500

    test
    Signed-off-by: David C <dchoi1022@gmail.com>

--- flaskapi/myflask01.py
+++ flaskapi/myflask01.py
@@ -9,12 +9,12 @@ app = Flask(__name__)
 # route() function of the Flask class is a
 # decorator, tells the application which URL
 # should call the associated function
-#@app.route("/")
-@app.route("/hello")
+@app.route("/")
+#@app.route("/hello")
 #@app.route("/slappy")
 def hello_world():
    return "Hello World\n"
 
 if __name__ == "__main__":
-   #app.run(host="0.0.0.0", port=2224) # runs the application
-   app.run(host="0.0.0.0", port=2224, debug=True) # DEBUG MODE
+   app.run(host="0.0.0.0", port=2224) # runs the application
+   #app.run(host="0.0.0.0", port=2224, debug=True) # DEBUG MODE
